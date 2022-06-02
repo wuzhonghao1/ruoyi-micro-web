7a00639190536c7a42dc2da88c74892d83fe7ae9
wuzhonghao <1076796500@qq.com>
2022-06-02 10:40:05 +0800

    初始化

---
+++ init.bat
@@ -0,0 +1,3 @@
+start cmd /k "cd vuedemo && yarn install"
+start cmd /k "cd reactdemo && yarn install"
+start cmd /k "cd vuemain && yarn install"
---
+++ start.bat
@@ -0,0 +1,3 @@
+start cmd /k "cd vuedemo && npm start"
+start cmd /k "cd reactdemo && npm start"
+start cmd /k "cd vuemain && npm start"
